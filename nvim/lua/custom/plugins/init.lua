5bdd9021d53d46d83d65f094958944ece49a5cd5
mxg1410 <mxg1410@qsl.com.au>
2024-04-01 11:42:07 +1000

    Add harpoon and keybinds

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -14,11 +14,64 @@ return {
     end,
   },
   {
-    'ray-x/lsp_signature.nvim',
-    event = 'VeryLazy',
-    opts = {},
-    config = function(_, opts)
-      require('lsp_signature').setup(opts)
+    'ThePrimeagen/harpoon',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('harpoon').setup {}
+      vim.api.nvim_set_keymap('n', '<leader>hh', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>',
+        {
+          desc = '[h]arpoon: Toggle Quick Menu',
+          noremap = true,
+          silent = true
+        })
+
+      vim.api.nvim_set_keymap('n', '<leader>ha', '<cmd>lua require("harpoon.mark").add_file()<CR>',
+        {
+          desc = 'Harpoon: [a]dd file',
+          noremap = true,
+          silent = true
+        })
+
+      vim.api.nvim_set_keymap('n', '<leader>h1', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>',
+        {
+          desc = 'Harpoon: [1] nav file',
+          noremap = true,
+          silent = true
+        })
+      vim.api.nvim_set_keymap('n', '<leader>h2', '<cmd>lua require("harpoon.ui").nav_file(2)<CR>',
+        {
+          desc = 'Harpoon: [2] nav file',
+          noremap = true,
+          silent = true
+        })
+      vim.api.nvim_set_keymap('n', '<leader>h3', '<cmd>lua require("harpoon.ui").nav_file(3)<CR>',
+        {
+          desc = 'Harpoon: [3] nav file',
+          noremap = true,
+          silent = true
+        })
+      vim.api.nvim_set_keymap('n', '<leader>h4', '<cmd>lua require("harpoon.ui").nav_file(4)<CR>',
+        {
+          desc = 'Harpoon: [4] nav file',
+          noremap = true,
+          silent = true
+        })
+
+      vim.api.nvim_set_keymap('n', '<leader>h<CR>', '<cmd>lua require("harpoon.ui").nav_file(1)<CR>',
+        {
+          desc = 'Harpoon: First file',
+          noremap = true,
+          silent = true
+        })
+
+      vim.api.nvim_set_keymap('n', '<leader>ht', '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>',
+        {
+          desc = 'Harpoon: [t]erminal',
+          noremap = true,
+          silent = true
+        })
     end,
   },
 }
