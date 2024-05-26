89a24358b513b10ba29074c7af1e0189228e8202
Michael Galloway <stick.mick@gmail.com>
2024-05-26 19:59:39 +1000

    Ricing

--- hypr/start.sh
+++ hypr/start.sh
@@ -3,7 +3,9 @@
 # initializing wallpaper daemon
 swww init &
 # setting wallpaper
-# swww img ~/Wallpapers/image.png &
+swww img ~/Pictures/Wallpapers/sunset-mountain-beautiful-digital-art-scenery-4k-wallpaper-uhdpaper.com-183@1@n.jpg &
+
+wal -i "~/Pictures/Wallpapers/sunset-mountain-beautiful-digital-art-scenery-4k-wallpaper-uhdpaper.com-183@1@n.jpg"
 
 nm-applet --indicator &
 
