d7f3332c8d8ff467253234fe662a1b27c01f4c3a
Michael Galloway <stick.mick@gmail.com>
2024-10-06 18:59:01 +1000

    Update lock screen and monitor shutdown

--- hypr/start.sh
+++ hypr/start.sh
@@ -5,10 +5,12 @@ swww init &
 # setting wallpaper
 swww img ~/Pictures/Wallpapers/sunset-mountain-beautiful-digital-art-scenery-4k-wallpaper-uhdpaper.com-183@1@n.jpg &
 
-wal -i "~/Pictures/Wallpapers/sunset-mountain-beautiful-digital-art-scenery-4k-wallpaper-uhdpaper.com-183@1@n.jpg"
+wal -i "~/Pictures/Wallpapers/sunset-mountain-beautiful-digital-art-scenery-4k-wallpaper-uhdpaper.com-183@1@n.jpg" &
 
 nm-applet --indicator &
 
-waybar
+waybar &
 
-dunst
+dunst &
+
+swayidle -w timeout 150 'swaylock -f' timeout 200 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
