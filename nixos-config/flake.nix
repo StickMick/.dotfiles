855de34511ec456634a4662856ce724d5f683c68
Stick Mick <stick.mick@gmail.com>
2025-11-25 08:31:45 +1000

    Refactor flake configuration: remove unused inputs and improve structure

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -5,16 +5,11 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
-    nvf = {
-      url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs@{
     self,
     nixpkgs,
-    nvf,
     ...
   }: let
     inherit (self) outputs;
@@ -28,22 +23,7 @@
         };
         modules = [
           ./system/core.nix
-          {
-            specialisation = {
-              kde.configuration = import ./kde/kde.nix;
-              cosmic.configuration = import ./cosmic/cosmic.nix;
-              hyprland.configuration = import ./hyprland/hyprland.nix;
-            };
-          }
-        ];
-      };
-      wsl = lib.nixosSystem {
-        specialArgs = {
-          inherit inputs system;
-        };
-        modules = [
-          ./wsl/wsl.nix
-          nvf.nixosModules.default
+          ./kde/kde.nix
         ];
       };
     };
